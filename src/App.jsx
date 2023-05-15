704ee89701bf667218291fea4d16b61c84a5daeb
iamatos3 <matosian93@gmail.com>
2023-05-14 23:25:02 -0400

    Installed all dependencies and imported all data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+import people from './data';
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = people[index];
+  console.log(name);
+
+  return (
+    <h2>Review Starter</h2>
+  );
 };
 export default App;
